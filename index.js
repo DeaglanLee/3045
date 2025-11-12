c65d8b80efac6d5945aa6b85f62cf897df61c5e8
Connor McKenzie <connora.mckenzie@gmail.com>
2025-11-12 15:36:24 +0000

    Add screens and get reqs

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ const express = require("express");
 const app = express();
 const path = require('path');
 
+const port = 3000;
+
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
 
-// app.gets
 app.get('/', async (req, res) => {
     let title = "Home";
-    let sess_obj = req.session;
-    try {
-        res.render("", { titledata: title })
-    } catch (error) {
-        sess_obj.isLoggedIn = false;
-        res.render("", { titledata: title })
-    }
+    res.render("pages/home", { titledata: title })
+});
+
+app.get("/login", async (req, res) => {
+    let title = "Login";
+    res.render("pages/login", { titledata: title });
+});
+
+app.get("/register", async (req, res) => {
+    let title = "Register";
+    res.render("pages/register", { titledata: title });
 });
