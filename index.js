4e2abe1d140fc79d5fc3e125dc136c6ba39bf3e0
Connor McKenzie <connora.mckenzie@gmail.com>
2025-11-12 17:02:37 +0000

    Implemented login + register endpoints

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const { registerUser } = require("./auth");
+const { stat } = require("fs");
+const { isStrongPassword, isValidEmail } = require("./registration/validation");
 
 const port = 3000;
 
@@ -30,21 +32,38 @@ app.get("/register", async (req, res) => {
 });
 
 
-
-
 // POSTS
-app.post("/register", async (req, res) => {
-    const email = req.body.emailInput
-    const password = req.body.passwordInput
-    registerUser(username, password)
-    
+app.post("/registerUser", async (req, res) => {
+    const email = req.body.emailInput;
+    const password = req.body.passwordInput;
 
-    res.render("/");
+    if (!isValidEmail(email)) {
+        res.send(JSON.stringify({
+            status: 400,
+            message: "Invalid email format."
+        }));
+        return;
+    }
+    
+    if (!isStrongPassword(password)) {
+        res.send(JSON.stringify({
+            status: 400,
+            message: "Password is not strong enough."
+        }));
+        return;
+    }
+
+    const { status } = await registerUser(email, password);
+    const message = status === 201 ? "Registration successful." : "Registration failed.";
+
+    res.send(JSON.stringify({ status, message }));
 });
 
-app.post("/login", async (req, res) => {
-    const email = req.body.emailInput
-    const password = req.body.passwordInput    
+app.post("/loginUser", async (req, res) => {
+    const { email, password } = req.body; 
+    
+    const { status } = await usernamePasswordMatches(email, password);
+    const message = status === 200 ? "Login successful." : "Login failed.";
 
-    res.redirect("/");
+    res.send(JSON.stringify({ status, message }));
 });
