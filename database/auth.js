8ad236e5b4ce5865666bf8709428eecdd2883b43
Michelle0204 <126016121+Michelle0204@users.noreply.github.com>
2025-11-24 13:18:57 +0000

    including username in db

--- database/auth.js
+++ database/auth.js
@@ -3,13 +3,14 @@ import bcrypt from 'bcrypt';
 import { db } from './client.js';
 const database = await db();
 
-async function registerUser(email, plainPassword) {
+async function registerUser(email, username, plainPassword) {
   const salt = 10; //reduce dcrypt hash length to 10
   const users = database.collection("users"); //connect to db and access "users" collection
   const hashedPassword = await bcrypt.hash(plainPassword, salt);
 
   const doc = {
     email, 
+    username,
     hashedPassword,
     registeredAt: new Date()
   };
